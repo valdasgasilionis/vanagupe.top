e5cec5e1471d9d2162688e772610eab9fbd57c55
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-06-23 09:58:42 +0300

    s3 test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Vanagupe.top
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="http://images.vanagupe.top/logo.png">Docs</a>
                 </div>
             </div>
         </div>
