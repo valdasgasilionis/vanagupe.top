3c5d8b99eed20a7abe2a6379fbe9607fbe5663d5
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-06-23 20:20:56 +0300

    list buckets

---
+++ app/Http/Controllers/S3Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Aws\S3\S3Client;  
+use Aws\Exception\AwsException;
+use Illuminate\Http\Request;
+
+class S3Controller extends Controller
+{
+    public function list() {
+        $s3Client = new S3Client([
+            'profile' => 'default',
+            'region' => 'eu-west-1',
+            'version' => '2006-03-01'
+        ]);
+        
+        //Listing all S3 Bucket
+        $buckets = $s3Client->listBuckets();
+        foreach ($buckets['Buckets'] as $bucket) {
+            echo $bucket['Name'] . "\n";
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/s3list', 'S3Controller@list');
