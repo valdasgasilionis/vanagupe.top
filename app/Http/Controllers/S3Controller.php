0f7cc5f3b15146cc48cada8add4e4d3ca82e56fb
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-06-27 13:57:07 +0300

    multipart low level

--- app/Http/Controllers/S3Controller.php
+++ app/Http/Controllers/S3Controller.php
@@ -66,6 +66,63 @@ class S3Controller extends Controller
         } catch (MultipartUploadException $e) {
         echo $e->getMessage() . PHP_EOL;
         }
-
+    }
+    public function lowlevelmultipart() {
+        $bucket = 'dokumentai.vanagupe.top';
+        $keyname = 'archive.zip';
+$filename = 'Photos.zip';
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region' => 'eu-west-1'
+   ]);
+$result = $s3->createMultipartUpload([
+ 'Bucket' => $bucket,
+ 'Key' => $keyname,
+ 'StorageClass' => 'Standard',
+ 'ACL' => 'public-read',
+ /* 'Metadata' => [
+ 'param1' => 'value 1',
+ 'param2' => 'value 2',
+ 'param3' => 'value 3'
+ ] */
+]);
+$uploadId = $result['UploadId'];
+// Upload the file in parts.
+try {
+ $file = fopen($filename, 'r');
+ $partNumber = 1;
+ while (!feof($file)) {
+ $result = $s3->uploadPart([
+ 'Bucket' => $bucket,
+ 'Key' => $keyname,
+ 'UploadId' => $uploadId,
+ 'PartNumber' => $partNumber,
+ 'Body' => fread($file, 1 * 1024 * 1024),
+ ]);
+ $parts['Parts'][$partNumber] = [
+ 'PartNumber' => $partNumber,
+ 'ETag' => $result['ETag'],
+ ];
+ $partNumber++;
+ echo "Uploading part {$partNumber} of {$filename}." . PHP_EOL;
+ }
+ fclose($file);
+} catch (S3Exception $e) {
+ $result = $s3->abortMultipartUpload([
+ 'Bucket' => $bucket,
+ 'Key' => $keyname,
+ 'UploadId' => $uploadId
+ ]);
+ echo "Upload of {$filename} failed." . PHP_EOL;
+}
+// Complete the multipart upload.
+$result = $s3->completeMultipartUpload([
+ 'Bucket' => $bucket,
+ 'Key' => $keyname,
+ 'UploadId' => $uploadId,
+ 'MultipartUpload' => $parts,
+]);
+$url = $result['Location'];
+echo "Uploaded {$filename} to {$url}." . PHP_EOL;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/', function () {
 Route::get('/s3list', 'S3Controller@list');
 Route::get('/s3store', 'S3Controller@store');
 Route::get('/s3multipart', 'S3Controller@multipart');
+Route::get('/s3lowlevelmultipart', 'S3Controller@lowlevelmultipart');
