e1a23331c3dde694a2752120799101952d4b3f47
=Valdas Gasilionis <=valdasgasilionis@yahoo.com>
2019-07-01 14:36:24 +0300

    import class

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Redirect;
 
 class RegisterController extends Controller
 {
@@ -38,7 +39,7 @@ class RegisterController extends Controller
     public function __construct()
     {
        /*  $this->middleware('guest'); */
-       return redirect('/')->with('register', 'Registration is for admin only!');
+       Redirect::to('/')->send();
     }
 
     /**
